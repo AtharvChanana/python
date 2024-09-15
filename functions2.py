24cbec82e57f1747c4947438349e380047a37e5b
Atharv Chanana <raaghav5616@gmail.com>
2024-09-15 21:34:52 +0530

    Create functions2.py

---
+++ functions2.py
@@ -0,0 +1,8 @@
+def even_num(li):
+    even_num_list=[]
+    for num in li:
+        if num%2==0:
+            return even_num_list.append(num)
+        else:
+            pass
+    return even_num_list
